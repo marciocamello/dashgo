27af771d4340f4bee2e2edf07228dfbe3a6898f9
marciocamello <marciocamello@oulook.com>
2022-03-19 19:48:12 +0000

    feat: add header, sidebar, list

---
+++ src/components/Header.tsx
@@ -0,0 +1,96 @@
+import { Avatar, Box, Flex, HStack, Icon, Input, Text } from '@chakra-ui/react';
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri';
+
+
+export function Header() {
+    return (
+        <Flex
+            as="header"
+            w="100%"
+            maxW={1480}
+            h="20"
+            mx="auto"
+            mt="4"
+            px="4"
+            align="center"
+        >
+            <Text
+                fontSize="3xl"
+                fontWeight="bold"
+                letterSpacing="tight"
+                w="64"
+            >
+                DashGO
+                <Text color="pink.500" as="span" ml="1">.</Text>
+            </Text>
+
+            <Flex
+                as="label"
+                flex="1"
+                py="4"
+                px="8"
+                ml="6"
+                maxWidth={400}
+                alignSelf="center"
+                color="gray.200"
+                position="relative"
+                backgroundColor="gray.800"
+                borderRadius="full"
+            >
+                <Input
+                    color="gray.50"
+                    variant="unstyled"
+                    px="4"
+                    mr="4"
+                    placeholder="Search"
+                    _placeholder={{
+                        color: "gray.400",
+                    }}
+                />
+
+                <Icon
+                    as={RiSearchLine}
+                    fontSize="20"
+                />
+            </Flex>
+
+            <Flex
+                align="center"
+                ml="auto"
+            >
+                <HStack
+                    spacing={4}
+                    mx="8"
+                    pr="8"
+                    py="1"
+                    color="gray.300"
+                    borderRightWidth={1}
+                    borderColor="gray.700"
+                >
+                    <Icon as={RiNotificationLine} fontSize="20" />
+                    <Icon as={RiUserAddLine} fontSize="20" />
+                </HStack>
+
+                <Flex
+                    align="center"
+                >
+                    <Box
+                        mr="4"
+                        textAlign="right"
+                    >
+                        <Text>Marcio Camello</Text>
+                        <Text color="gray.300" fontSize="small">
+                            mac3designer@gmail.com
+                        </Text>
+                    </Box>
+
+                    <Avatar
+                        size="md"
+                        name="Marcio Camello"
+                        src="https://github.com/marciocamello.png"
+                    />
+                </Flex>
+            </Flex>
+        </Flex>
+    )
+}
---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,106 @@
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
+import { ApexOptions } from "apexcharts";
+import dynamic from "next/dynamic";
+import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
+
+const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
+
+const options = {
+    chart: {
+        toolbar: {
+            show: false
+        },
+        zoom: {
+            enabled: false
+        },
+        foreColor: theme.colors.gray[500]
+    },
+    grid: {
+        show: false
+    },
+    dataLabels: {
+        enabled: false
+    },
+    tooltip: {
+        enabled: false
+    },
+    xaxis: {
+        type: "datetime",
+        axisBorder: {
+            color: theme.colors.gray[600]
+        },
+        axisTicks: {
+            color: theme.colors.gray[600]
+        },
+        categories: [
+            '2021-01-01T00:00:00.000Z',
+            '2021-01-02T00:00:00.000Z',
+            '2021-01-03T00:00:00.000Z',
+            '2021-01-04T00:00:00.000Z',
+            '2021-01-05T00:00:00.000Z',
+            '2021-01-06T00:00:00.000Z',
+            '2021-01-07T00:00:00.000Z',
+        ]
+    },
+    fill: {
+        opacity: 0.3,
+        type: 'gradient',
+        gradient: {
+            shade: 'dark',
+            opacityFrom: 0.7,
+            opacityTo: 0.3,
+        }
+    }
+} as ApexOptions;
+
+const series = [
+    {
+        name: 'series-1',
+        data: [31, 40, 28, 51, 42, 109, 100]
+    }
+];
+
+export default function Dashboard() {
+    return (
+        <Flex direction="column" h="100vh" >
+            <Header />
+
+            <Flex width="100%" my="6" maxWidth={1480} mx="auto" px="6">
+                <Sidebar />
+
+                <SimpleGrid
+                    flex="1"
+                    gap="4"
+                    minChildWidth="320px"
+                    alignItems="flex-start"
+                >
+                    <Box
+                        p="8"
+                        bg="gray.800"
+                        borderRadius={8}
+                    >
+                        <Text fontSize="lg" mb="4">Inscritos da semana</Text>
+                        <Chart
+                            options={options}
+                            series={series}
+                            type="area"
+                        />
+                    </Box>
+                    <Box
+                        p="8"
+                        bg="gray.800"
+                        borderRadius={8}
+                    >
+                        <Text fontSize="lg" mb="4">Taxa de abertura</Text>
+                        <Chart
+                            options={options}
+                            series={series}
+                            type="area"
+                        />
+                    </Box>
+                </SimpleGrid>
+            </Flex>
+        </Flex >
+    )
+}
